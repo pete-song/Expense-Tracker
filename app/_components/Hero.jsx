a6fe1115a9a171886fba69cc2a75c369ae7947ec
pattapon-son <pattapon.son@student.mahidol.edu>
2025-03-19 19:03:38 +0700

    Updated dashboard image

--- app/_components/Hero.jsx
+++ app/_components/Hero.jsx
@@ -25,7 +25,7 @@ function Hero() {
             </div>
             </div>
         </div>
-        <Image src="/dashboard-template.png" alt='dashboard' width={1000} height={700} className="-mt-9 rounded-xl border-2 "/>
+        <Image src="/dashboard.png" alt='dashboard' width={1000} height={700} className="-mt-9 rounded-xl border-2 "/>
     </section>
   )
 }
